260493f3257a355695b37eecea225f797d0c259e
huangml <huangml.zh@gmail.com>
2014-12-08 16:47:22 +0800

    config API

--- api_v1.go
+++ api_v1.go
@@ -23,6 +23,8 @@ func (h *handlerV1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		h.keys(w, r)
 	} else if strings.HasPrefix(r.URL.Path, "/v1/status") {
 		h.status(w)
+	} else if strings.HasPrefix(r.URL.Path, "/v1/config") {
+		h.config(w, r)
 	} else {
 		http.NotFound(w, r)
 	}
@@ -66,4 +68,16 @@ func (h *handlerV1) put(w http.ResponseWriter, key string, value []byte, ttw int
 
 func (h *handlerV1) status(w http.ResponseWriter) {
 	w.Write(h.p.Status())
+}
+
+func (h *handlerV1) config(w http.ResponseWriter, r *http.Request) {
+	loader, _ := strconv.Atoi(r.URL.Query().Get("loader"))
+	saver, _ := strconv.Atoi(r.URL.Query().Get("saver"))
+	if loader > 0 {
+		h.p.SetLoadMaxProc(loader)
+	}
+	if saver > 0 {
+		h.p.SetSaveProc(saver)
+	}
+	w.WriteHeader(http.StatusOK)
 }
